1992828aa37018b8be6a5e3ccc4600e9ae7c174e
junior-iv <juniorrr.iv@gmail.com>
2025-06-26 13:42:30 +0300

    5.25.2

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -205,8 +205,8 @@ function convertJSONToTableFoLogLikelihood(jsonData) {
     let table = `<details class="w-80 my-4 h-100 h7" open><summary>Log-likelihood information</summary>
                         <table class="w-97 table-borderless">`;
     Object.entries(jsonData).forEach(([key, value]) => {
-        table += `<tr><th class="w-auto text-center text-break alert alert-info bg-opacity-50">${key}</th>`;
-        table += `<th class="w-auto text-center text-break alert alert-info bg-opacity-75">${value}</th></tr>`;
+        table += `<tr><th class="w-auto text-center toast-body alert alert-info opacity-25">${key}</th>`;
+        table += `<th class="w-auto text-center toast-body alert alert-info opacity-25">${value}</th></tr>`;
     });
     table += `</table></details>`;
     document.getElementById('logLikelihood').innerHTML = table;
@@ -218,9 +218,9 @@ function convertJSONToTableFoFileList(jsonData) {
     let firstRow = ``;
     let secondRow = ``;
     Object.entries(jsonData).forEach(([key, value]) => {
-        headersRow += `<th  class="text-center text-danger w-auto text-break alert alert-info bg-opacity-75">${key}</th>`;
-        firstRow += `<th  class="w-auto text-center text-break alert alert-info bg-opacity-50">${value[0]}</th>`;
-        secondRow += `<th class="w-auto text-center text-break alert alert-info bg-opacity-75">${value[1]}</th>`;
+        headersRow += `<th  class="text-center text-danger w-auto toast-body alert alert-info opacity-75">${key}</th>`;
+        firstRow += `<th  class="w-auto text-center toast-body alert alert-info opacity-50">${value[0]}</th>`;
+        secondRow += `<th class="w-auto text-center toast-body alert alert-info opacity-75">${value[1]}</th>`;
     });
     let table = `<details class=" my-4 w-80 h-100 h7" open><summary>File list</summary>
              <table class="w-97 table-borderless "><tr>${secondRow}</tr><tr>${headersRow}</tr><tr>${firstRow}</tr>
