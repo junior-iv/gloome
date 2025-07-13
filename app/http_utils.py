4cd273ee358223088446c7e2620050c47cc30ad8
junior-iv <juniorrr.iv@gmail.com>
2025-07-13 15:07:18 +0300

    5.25.32k

--- app/http_utils.py
+++ app/http_utils.py
@@ -10,6 +10,10 @@ def get_response(process_id: int) -> Any:
     conf = WebConfig(PROCESS_ID=process_id)
     try:
         result = conf.read_response()
+        with open(f'/var/www/vhosts/gloomedev.tau.ac.il/httpdocs/tmp/test_route_debug.log', 'a') as f:
+            f.write(f'\n\n--- get_response ---\n')
+            f.write(f'{process_id}')
+            f.write(result)
     except Exception:
         conf.set_job_logger_info(traceback.format_exc())
         with open(f'/var/www/vhosts/gloomedev.tau.ac.il/httpdocs/tmp/{conf.CURRENT_JOB}_{conf.PROCESS_ID}_'
