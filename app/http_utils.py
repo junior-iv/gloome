65c67bb08e47de6bdf79a3a3e407db4787386da1
junior-iv <juniorrr.iv@gmail.com>
2025-07-14 17:30:43 +0300

    5.25.32w

--- app/http_utils.py
+++ app/http_utils.py
@@ -11,7 +11,7 @@ def get_response(process_id: int) -> Any:
     conf = WebConfig(PROCESS_ID=process_id)
     conf.texts_filling()
     try:
-        result = json.loads(conf.read_response())
+        result = jsonify(message=conf.read_response()).response
     except Exception:
         conf.set_job_logger_info(traceback.format_exc())
         with open(f'/var/www/vhosts/gloomedev.tau.ac.il/httpdocs/tmp/{conf.CURRENT_JOB}_{conf.PROCESS_ID}_'
