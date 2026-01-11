f5590efc3a53742605ee23a735f12beaa9c3fd96
junior-iv <juniorrr.iv@gmail.com>
2026-01-11 15:55:19 +0200

    5.25.67

--- app/http_utils.py
+++ app/http_utils.py
@@ -89,7 +89,7 @@ def read_json(json_string: str) -> Any:
 def send_report() -> Any:
     try:
         mail = MailSenderSMTPLib(name='execution reports runs')
-        date = datetime.date.today() - datetime.timedelta(days=3)
+        date = datetime.date.today() - datetime.timedelta(days=1)
         start_datetime = datetime.datetime(date.year, date.month, date.day, 0, 0, 0, 0).timestamp()
         end_datetime = datetime.datetime(date.year, date.month, date.day, 23, 59, 59, 999999).timestamp()
         mail.send_log_files_list(start_date=start_datetime, end_date=end_datetime)
