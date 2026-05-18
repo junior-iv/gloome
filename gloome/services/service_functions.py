1c8103e925437cd2e6b8a862f4b3a10385023aab
junior-iv <juniorrr.iv@gmail.com>
2026-05-18 16:55:22 +0300

    5.26.93

--- gloome/services/service_functions.py
+++ gloome/services/service_functions.py
@@ -448,8 +448,8 @@ def get_response_design(json_object: Optional[Any], action_name: str, create_lin
     if 'create_all_file_types' in action_name and create_link:
         if output_file:
             json_object.update({'json response file (json)': output_file})
-        json_object = link_design(json_object)
-        json_object = result_design(json_object, change_value='compute_likelihood_of_tree' in action_name,
+
+        json_object = result_design(link_design(json_object), change_value='compute_likelihood_of_tree' in action_name,
                                     change_value_style=False, change_key=True, change_key_style=False)
     return json_object
 
